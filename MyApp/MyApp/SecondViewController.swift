c90280669d7adecdc7c482a41c2690ce2115b655
SuperLazyDog <jyoitatsu@foxmail.com>
2017-04-29 01:33:32 +0900

    test

---
+++ MyApp/MyApp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MyApp
+//
+//  Created by 徐伟达 on 2017/4/29.
+//  Copyright © 2017年 徐伟达. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
