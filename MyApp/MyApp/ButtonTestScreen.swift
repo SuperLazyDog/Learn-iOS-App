f70840eb338a9253388a9522f8c34c91b8b89e04
SuperLazyDog <jyoitatsu@foxmail.com>
2017-05-09 19:05:36 +0900

    stepper test

--- MyApp/MyApp/ButtonTestScreen.swift
+++ MyApp/MyApp/ButtonTestScreen.swift
@@ -9,29 +9,54 @@
 import UIKit
 
 class ButtonTestScreen: UIViewController {
+    //------------------------------------------------------------------------
+    //                               プロパティ
+    //------------------------------------------------------------------------
+    //-------------------------------------------------------------------
+    //                 　　　　　ローカルプロパティ
+    //-------------------------------------------------------------------
+    //-------------------------------------------------------------------
+    //                 　　　　　　　UI
+    //-------------------------------------------------------------------
+    @IBOutlet weak var valueLabel: UILabel!
+    @IBOutlet weak var clearValueButton: UIButton!
+    @IBOutlet weak var valueStepper: UIStepper!
+    //------------------------------------------------------------------------
+    //                                 関数
+    //------------------------------------------------------------------------
+    //-------------------------------------------------------------------
+    //                 　　　　　カスタマイズ関数
+    //-------------------------------------------------------------------
     func ok(_ sender: UIButton) {
         print("OK")
     }
+    //-------------------------------------------------------------------
+    //              　　　　　　 ViewController関数
+    //-------------------------------------------------------------------
+    //------------------------------------------------------
+    //                     viewDidLoad
+    //------------------------------------------------------
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         let okButton = UIButton(type: .custom)
         okButton.frame = CGRect(x: 100, y: 100, width: 120, height: 120)
+        //------------------------------------------------------------------------
+        //                       　コードでボタンを追加
+        //------------------------------------------------------------------------
         //let bkgImage = UIImage(named: "maru")
-        okButton.setBackgroundImage(#imageLiteral(resourceName: "Image-2"), for: .normal)
+        /*okButton.setBackgroundImage(#imageLiteral(resourceName: "Image-2"), for: .normal)
         okButton.setImage(#imageLiteral(resourceName: "Image-1"), for: .highlighted)
         okButton.setTitle("OK", for: .normal)
         okButton.setTitleColor(UIColor.black, for: .normal)
-        
         okButton.addTarget(self, action: #selector(ok(_:)), for: .touchUpInside)
-        
         okButton.isHidden = false
-        
-        
-        view.addSubview(okButton)
+        view.addSubview(okButton)*/
     }
-
+    //------------------------------------------------------
+    //               didReceiveMemoryWarning
+    //------------------------------------------------------
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
