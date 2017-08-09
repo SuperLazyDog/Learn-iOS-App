429cbd653f6e87b4add7761e6c59fc97bed518d4
SuperLazyDog <jyoitatsu@foxmail.com>
2017-08-09 17:44:18 +0900

    导航控制器用法

--- Navigation-Demo/Navigation-Demo/ViewController.swift
+++ Navigation-Demo/Navigation-Demo/ViewController.swift
@@ -1,26 +1,18 @@
 //
 //  ViewController.swift
-//  Learning-PureLayout
+//  Navigation-Demo
 //
 //  Created by Weida Xu on 2017/8/9.
 //  Copyright © 2017年 Weida Xu. All rights reserved.
 //
 
 import UIKit
-import PureLayout
 
 class ViewController: UIViewController {
-    let label = UILabel();
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        label.text = "test"
-        
-        view.addSubview(label)
-        
-        label.autoAlignAxis(toSuperviewAxis: .horizontal)
-        
     }
 
     override func didReceiveMemoryWarning() {
