b1e7f91893ec69b1cb36be32a952cc1a410ff6db
SuperLazyDog <jyoitatsu@foxmail.com>
2018-05-03 08:50:53 +0900

    Done-step: [13.9 UICollectionViewController]

---
+++ pro/pro/Views/S13ManageViewScreen/CollectionViewController/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  pro
+//
+//  Created by 徐伟达 on 2018/5/1.
+//  Copyright © 2018年 Weida. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+	@IBOutlet weak var titleLabel: UILabel!
+}
