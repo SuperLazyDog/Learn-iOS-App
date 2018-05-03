1fd3b37514c856b1462b12e21611f622a1eaa634
SuperLazyDog <jyoitatsu@foxmail.com>
2018-05-03 10:08:01 +0900

    Dones-step: [13.10 UIPageViewController]

---
+++ pro/pro/Views/S13ManageViewScreen/PageViewController/PageViewController.swift
@@ -0,0 +1,66 @@
+//
+//  PageViewController.swift
+//  pro
+//
+//  Created by 徐伟达 on 2018/5/3.
+//  Copyright © 2018年 Weida. All rights reserved.
+//
+
+import UIKit
+
+class PageViewController: UIPageViewController {
+	let identifierIDs = ["PageViewControllerFirst", "PageViewControllerSecond", "PageViewControllerThird"]
+	var targetViewControllers: [UIViewController] = []
+    override func viewDidLoad() {
+		// 设置dataSource, delegate
+		self.dataSource = self
+		self.delegate = self
+        super.viewDidLoad()
+		// 设置pages
+		self.targetViewControllers = []
+		for id in self.identifierIDs {
+			self.targetViewControllers.append((self.storyboard?.instantiateViewController(withIdentifier: id))!)
+		}
+		// 设置一开始显示的viewController
+		setViewControllers([targetViewControllers[0]], direction: .forward, animated: true)
+		
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension PageViewController: UIPageViewControllerDataSource {
+	// 设置当前页面的前一页
+	func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+		if let index = self.targetViewControllers.index(of: viewController), index > 0 {
+			return self.targetViewControllers[index - 1]
+		}
+		return nil
+	}
+	// 设置当前页面的后一页
+	func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+		if let index = self.targetViewControllers.index(of: viewController), index < self.targetViewControllers.count - 1  {
+			return self.targetViewControllers[index + 1]
+		}
+		return nil
+	}
+
+}
+
+extension PageViewController: UIPageViewControllerDelegate {
+}
